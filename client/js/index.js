66e2a403ef532e56876cd032fd70cb2a8191dc12
Ionică Bizău <bizauionica@yahoo.com>
2015-05-20 13:57:57 +0300

    Toggle the new repo popup

--- client/js/index.js
+++ client/js/index.js
@@ -1,13 +1,27 @@
 var $ = require("jquery");
-(function (root) {
-    function Ui() {}
-    Ui.Repository = function () {}
-    Ui.Repository.prototype.popup = function (show) {
 
-    };
-    Ui.repository = new Ui.Repository();
-    function GitHub() {
-    }
-    GitHub.ui = new Ui();
-    this.GitHub = new GitHub();
-})(this);
+$(document).ready(function () {
+    (function (root) {
+        function Ui() {
+            this.repository = new Ui.Repository();
+        }
+        Ui.Repository = function () {
+            this._ = $(".popup-create-new-repository");
+        }
+        Ui.Repository.prototype.popup = function (f) {
+            this._[f]();
+        };
+
+        function GitHub() {
+            this.ui = new Ui();
+        }
+
+        this.GitHub = new GitHub();
+    })(window);
+
+    $(".create-repository").on("click", function () {
+        GitHub.ui.repository.popup("toggle");
+        return false;
+    });
+});
+
