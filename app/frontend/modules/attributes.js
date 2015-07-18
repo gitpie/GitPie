5c667862c485dfc336c51ba0bda4bad2c9ac5404
Matheus Paiva <matheus.a.paiva@gmail.com>
2015-07-18 09:00:33 -0300

    First test with loading angular attribute

--- app/frontend/modules/attributes.js
+++ app/frontend/modules/attributes.js
@@ -47,6 +47,35 @@
         });
       }
     };
+  })
+
+  .directive('ngLoading', function () {
+
+    return function(scope, element, attrs) {
+      var loadingContainer = angular.element([
+        '<div class="loading-container">',
+          '<svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">',
+             '<circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>',
+          '</svg>',
+        '</div>'
+      ].join(''));
+
+      element.append(loadingContainer);
+
+      scope.$watch(attrs.ngLoading, function(isLoading) {
+
+         if (isLoading) {
+           console.log(element);
+          //  img.removeClass('ng-hide');
+          //  element.addClass('loading');
+          //  element.attr('disabled', '');
+         } else {
+          //  img.addClass('ng-hide');
+          //  element.removeClass('loading');
+          //  element.removeAttr('disabled');
+         }
+      });
+    };
   });
 
 })();
