79bf143f7f05d34ded7f316c92ed7f7b88f68e7b
Matheus Paiva <matheus.a.paiva@gmail.com>
2015-12-10 00:02:52 -0200

    :bug: Fixes on emoji parse
    Fixed parse for two references of the same emoji and fixed parse for emojis :+1: e :-1:

--- app/core/emoji.js
+++ app/core/emoji.js
@@ -1,27 +1,33 @@
 var emojiList = require('../../resources/emoji/emojiList'),
   parse = function (string) {
-    // var emojis = string.match(/(:[\w+]+:)/g),
     var emojis = string.match(/(:[\w.&\-\+]+:)/g),
       parsedString = string;
 
     if (emojis) {
       var regex,
         emojiLabel,
-        emojiURL;
+        emojiURL,
+        dictionary = {};
 
       for (var i = 0; i < emojis.length; i++) {
-        emojiLabel = emojis[i].replace(/:/g, '');
-        regex = new RegExp(':[' + emojiLabel + ']+:', 'g');
-        emojiURL = emojiList[emojiLabel];
 
-        if (emojiURL) {
-          parsedString = parsedString.replace(regex, '<img class="emoji" src="' + emojiURL+ '" title="' + emojis[i] + '" />');
+        if (!dictionary[ emojis[i] ]) {
+          emojiLabel = emojis[i].replace(/:/g, '');
+          regex = new RegExp(':' + (emojiLabel.replace('+', '\\+').replace('-', '\\-')) + ':', 'g');
+          emojiURL = emojiList[emojiLabel];
+
+          if (emojiURL) {
+            parsedString = parsedString.replace(regex, '<img class="emoji" src="' + emojiURL+ '" title="' + emojis[i] + '" />');
+          }
+
+          dictionary[ emojis[i] ] = emojis[i];
         }
       }
 
       regex = null;
       emojiLabel = null;
       emojiURL = null;
+      dictionary = null;
     }
 
     return parsedString;
