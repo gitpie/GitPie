80b540e71c853ea400e02e2aef09e62168ab96f5
Matheus Paiva <matheus.a.paiva@gmail.com>
2016-01-20 03:41:47 -0200

    Test mergetool config for Windows

--- app/core/git.js
+++ app/core/git.js
@@ -839,7 +839,8 @@ Git.prototype.alterGitConfig = function (path, opts) {
     concatConfig('merge.tool', opts.mergeTool);
 
     if (wos.isWindows()) {
-      // TODO
+      concatConfig(`mergetool.${opts.mergeTool}.cmd`, `'${opts.mergeTool}.exe "$LOCAL" "$MERGED" "$REMOTE"'`);
+      concatConfig(`mergetool.${opts.mergeTool}.trustExitCode`, 'true');
     } else {
       concatConfig(`mergetool.${opts.mergeTool}.cmd`, `'${opts.mergeTool} "$LOCAL" "$MERGED" "$REMOTE"'`);
       concatConfig(`mergetool.${opts.mergeTool}.trustExitCode`, 'true');
